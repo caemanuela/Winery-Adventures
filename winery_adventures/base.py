45ab1da3ad6ce0f2cb66a7924b1cfc3063b07904
Emanuela <154596500+caemanuela@users.noreply.github.com>
2026-08-06 10:44:06 +0200

    feat: implement computations

--- winery_adventures/base.py
+++ winery_adventures/base.py
@@ -1,1 +1,8 @@
 #TODO: creare funzioni BaseWineryAnalyzer
+from abc import ABC, abstractmethod
+import polars as pl
+
+class BaseWineryAnalyzer(ABC):
+    @abstractmethod
+    def analyze_data(self, df: pl.DataFrame) -> pl.DataFrame:
+        pass
