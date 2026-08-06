0e40d6a2ced0879d2b2c2f07cf48dfade1a33d20
giadaorru <giadaorru05@gmail.com>
2026-08-06 10:24:46 +0200

    Fix temperature deviation logic and pass all unit tests

--- winery_adventures/base.py
+++ winery_adventures/base.py
@@ -1,1 +1,10 @@
-#TODO: creare funzioni BaseWineryAnalyzer
+from abc import ABC, abstractmethod
+import polars as pl
+
+class BaseWineryAnalyzer(ABC):
+
+    @abstractmethod
+    def analyze_data(self, df: pl.DataFrame) -> pl.DataFrame:
+        """Metodo astratto per analizzare o trasformare il DataFrame."""
+        pass
+    
